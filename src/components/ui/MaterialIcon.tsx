c896a40006758cf0c87a23990fbb1cdc5bbb4751
Stanley Luong <slluong@uw.edu>
2025-09-07 23:19:29 -0700

    remove woman female icon

--- src/components/ui/MaterialIcon.tsx
+++ src/components/ui/MaterialIcon.tsx
@@ -14,20 +14,6 @@ import SelfImprovementIcon from '@mui/icons-material/SelfImprovement'
 import SpaIcon from '@mui/icons-material/Spa'
 import WarningIcon from '@mui/icons-material/Warning'
 
-// Custom Female Icon Component
-const FemaleIcon: SvgIconComponent = (props) => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    height="24px" 
-    viewBox="0 -960 960 960" 
-    width="24px" 
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M440-120v-80h-80v-80h80v-84q-79-14-129.5-75.5T260-582q0-91 64.5-154.5T480-800q91 0 155.5 63.5T700-582q0 81-50.5 142.5T520-364v84h80v80h-80v80h-80Zm40-320q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41Z"/>
-  </svg>
-)
-
 interface MaterialIconProps {
   name: string
   className?: string
@@ -42,7 +28,7 @@ const iconMap: Record<string, SvgIconComponent> = {
   'moon': NightlightIcon,
   'candle': CandlestickChartIcon,
   'music': MusicNoteIcon,
-  'woman': FemaleIcon,
+  // 'woman': FemaleIcon,
   
   // Value icons
   'security': SecurityIcon,
